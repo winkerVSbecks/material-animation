f742557af42645712ec4704c77e0a4a3ad807be7
Varun Vachhar <varunvachhar@gmail.com>
2014-11-14 16:03:53 -0500

    just the basic animation in one view

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var colors = require('colors');
+var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+gulp.task('sass', function(done) {
+  gulp.src('./styles/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./styles/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./styles/'))
+    .pipe(connect.reload())
+    .on('end', done);
+});
+
+gulp.task('dev', function() {
+  // Start a server
+  connect.server({
+    root: '',
+    port: 3000,
+    livereload: true
+  });
+  console.log('[CONNECT] Listening on port 3000'.yellow.inverse);
+
+  // Watch HTML files for changes
+  console.log('[CONNECT] Watching files for live-reload'.blue);
+  watch({ glob: ['./index.html', './js/*.js']})
+    .pipe(connect.reload());
+
+  // Watch HTML files for changes
+  console.log('[CONNECT] Watching SASS files'.blue);
+  gulp.watch('./styles/*.scss', ['sass']);
+});
+
+gulp.task('default', [], function() {
+  console.log('***********************'.yellow);
+  console.log('  gulp dev'.yellow);
+  console.log('***********************'.yellow);
+  return true;
+});
---
+++ js/app.js
@@ -0,0 +1,79 @@
+
+angular.module('materialApp', [
+  'ngAnimate'
+])
+.controller('AnimationController', [
+  '$scope',
+  '$sniffer',
+  '$document',
+function($scope, $sniffer, $document, $window) {
+
+  $scope.state = 'right';
+
+  $scope.toggleCover = function(e) {
+    if ($scope.state === 'right') {
+      $scope.state = 'left';
+      createRipple(e.currentTarget);
+    } else {
+      $scope.state = 'right';
+      undoRipple(e.currentTarget);
+    }
+  };
+
+}]);
+
+var createRipple = function(target) {
+  var parentDiv = target.parentNode;
+  angular.element(parentDiv).find('svg').remove();
+
+  var offsets = target.getBoundingClientRect();
+  var x = offsets.left + offsets.width/2;
+  var y = offsets.top + offsets.height/2;
+
+  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+  var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      circle.setAttribute('cx', x);
+      circle.setAttribute('cy', y);
+      circle.setAttribute('r', 0);
+
+  var animate = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
+      animate.setAttribute('attributeType', 'XML');
+      animate.setAttribute('begin', '0s');
+      animate.setAttribute('attributeName', 'r');
+      animate.setAttribute('from', '0');
+      animate.setAttribute('to', '200%');
+      animate.setAttribute('dur', '600ms');
+      animate.setAttribute('fill', 'freeze');
+
+  circle.appendChild(animate);
+  svg.appendChild(circle);
+
+  parentDiv.insertBefore(svg, target);
+};
+
+
+var undoRipple = function(target) {
+  var parentDiv = target.parentNode;
+  var svg = parentDiv.getElementsByTagName('svg')[0];
+
+  if (!svg) return;
+
+  var circle = svg.getElementsByTagName('circle')[0];
+
+  var animate = circle.getElementsByTagName('animate')[0];
+  circle.removeChild(animate);
+
+  animate = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
+  animate.setAttribute('attributeType', 'XML');
+  animate.setAttribute('begin', 'indefinite');
+  animate.setAttribute('attributeName', 'r');
+  animate.setAttribute('from', '200%');
+  animate.setAttribute('to', '0');
+  animate.setAttribute('dur', '1200ms');
+  animate.setAttribute('fill', 'freeze');
+
+  circle.appendChild(animate);
+
+  animate.beginElement();
+};
